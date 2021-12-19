3c440dafef2d102d82283a31febfada338ffe078
Anastasiya <90135440+wisatta@users.noreply.github.com>
2021-12-19 14:00:16 +0300

    Загрузка файлов сайта

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('txt');
+btn.onclick = function (a) {
+    // a.preventDefault();
+    text.style.display = 'none';
+}
+
+
